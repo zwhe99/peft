66054e8a185b3aceb7fd3014a61dfd37b030bbc1
zwhe99 <hezw.tkcw@gmail.com>
2024-08-21 13:23:22 +0800

    add new loro_scaling_type option: ci for loro

--- src/peft/tuners/loro/layer.py
+++ src/peft/tuners/loro/layer.py
@@ -160,6 +160,10 @@ class LoroLayer(BaseTunerLayer):
                 nn.init.constant_(self.loro_mixing[adapter_name].weight[r - loro_left_rank:], loro_alpha / (effective_r - (r - loro_left_rank)))
 
                 self.scaling[adapter_name] = 1.0
+            elif loro_scaling_type == "ci":
+                nn.init.constant_(self.loro_mixing[adapter_name].weight, loro_alpha / effective_r)
+
+                self.scaling[adapter_name] = 1.0
             else:
                 raise ValueError(f"Unknown scaling type: {loro_scaling_type}")
         else:
